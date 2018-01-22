dfc6781a585f3233c85fe4844a59ef934bc5fba6
Jose <jose@webempresa.eu>
2018-01-22 02:27:11 +0100

    Empezamos

---
+++ Parte 3 - Variables/main.js
@@ -0,0 +1,9 @@
+//Tres formas declarar variables en ECMA6
+// let y var permite asignar de nuevo otro valor a la variable
+// const son como constantes
+let miVariable = 1;
+const miVariable2 = 2;
+
+var miVariable3;
+
+
